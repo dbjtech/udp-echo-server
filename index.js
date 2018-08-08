e022fa2e0b44cc8c46b7dc97122f38454cb58364
xiaoqiang0723 <457954719@qq.com>
2018-08-08 16:38:58 +0800

    添加最近一次休眠时间接口

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const logger = require('@brickyard/logger')
 const _ = require('lodash')
+const Koa = require('koa')
+const body = require('koa-body')
+const Koaqs = require('koa-qs')
 const Server = require('./server')
 const service = require('./service')
 const config = require('./config.js')
+const router = require('./koa-router')
 
 const server = new Server()
+const app = new Koa()
+app.use(body({ multipart: true, strict: false }))
+Koaqs(app)
 
 const DEFAULT_OPTIONS = {
 	port: 6688,
 }
 
 const port = _.get(config, 'web.port') || _.get(DEFAULT_OPTIONS, 'port')
+const host = _.get(config, 'web.host') || _.get(DEFAULT_OPTIONS, 'host')
 
 server.on('message', ({ buffer, reply }) => {
 	process.nextTick(() => {
@@ -28,6 +36,11 @@ async function start() {
 	logger.console.info('Listen @ %s', port)
 	logger.console.info('PID: %s', process.pid)
 	logger.console.info('TTY MODE: %s', !!process.stdout.isTTY)
+	app.use(router.router.routes())
+	app.use(router.router.allowedMethods())
+	app.listen(port, host, () => {
+		console.log('Start Http Server @ %s:%s', host, port)
+	})
 }
 
 start()
---
+++ koa-router/index.js
@@ -0,0 +1,48 @@
+const Router = require('koa-router')
+const ajv = require('ajv')({ useDefaults: true })
+const _ = require('lodash')
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const moment = require('moment')
+
+
+const adapter = new FileSync('echo_record.json')
+const lowdb = low(adapter)
+
+const router = new Router()
+
+const schema = {
+	properties: {
+		imei: { type: 'array', minItems: 1, items: { type: 'string' } },
+	},
+	required: ['imei'],
+}
+
+const validate = ajv.compile(schema)
+
+router.get('/get/echo/time', (ctx) => {
+	const data = _.mapKeys(ctx.query, (v, k) => _.camelCase(k))
+	const valid = validate(data)
+	if (!valid) {
+		ctx.body = {
+			status: 400,
+			message: 'paramter error',
+		}
+		return
+	}
+	const dataList = _.map(data.imei, v => lowdb.get('echo_records').find({ imei: v }).value())
+	_.forEach(dataList, (imei) => {
+		if (imei) {
+			imei.timesamp = moment(imei.timesamp * 1000).format('YYYY-MM-DD HH:mm:ss')
+		}
+	})
+	ctx.body = {
+		status: 200,
+		message: 'success',
+		data: dataList,
+	}
+})
+
+module.exports = {
+	router,
+}
